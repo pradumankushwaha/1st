e0696f6389f7d491c4d8cf9873438b5b83f4073a
pradumankushwaha <praduman.kushwaha.20.1@mountblue.tech>
2022-06-01 16:01:51 +0530

    This is a msg

---
+++ pp.py
@@ -0,0 +1,8 @@
+n=input("enter your name")
+p=int(input("Enter your roll number"))
+print(n,p)
+n1=int(input("Enter a number"))
+if(n1%2==0):
+	print("Its even number")
+else:	
+	print("Its odd number")
